c419f0d80c8dd00d214e16e5b643846c14ead9f3
liem_nguyen_thanh <liemdonduong@gmail.com>
2023-11-17 11:14:16 +0700

    chore: add loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
 import { SheetsPage } from "@/components/pages"
 
-const url = 'https://script.googleusercontent.com/a/macros/bita.jp/echo?user_content_key=0eE1jmrpuK-mcd2xL4uW_puwAqQWep0EpXaK7VssmP0L-7o6nqX8ME4V0mW-LTMfTLqxmRjw93-aNvl7tlVCwEA0p8Nl-joVm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_nRPgeZU6HP8d3hra3w8XUBVQhQ28DnQkJQL9R93UYiN6yYwVnDGfdKav5zPrJTegw-dYDdJIo-13eZTaZqANCXp3ctOaegirzTwHKX3qPpEq_pzm8kx7G50JyARCyWU63P0nD0x3y64&lib=Mku0QH6I7NxU3vji3EUa3jJJlMbEl9Nie'
-
-const getUsersNotPay = async () => {
-  const data = await fetch(url)
-  const response = await data.json()
-  return response
-}
-
 export default async function Page() {
-  const users = await getUsersNotPay()
 
   return (
-    <SheetsPage users={users} />
+    <SheetsPage />
   )
 }
--- src/components/pages/sheets-page.tsx
+++ src/components/pages/sheets-page.tsx
@@ -1,23 +1,65 @@
 'use client'
 import { convertToVND } from '@/utils/helper'
-import { Container, Stack, Typography } from '@mui/material'
+import { Box, CircularProgress, Container, Stack, Typography } from '@mui/material'
 import Link from 'next/link'
+import { Fragment, useEffect, useState } from 'react'
 
-type Props = {
-  users: {
-    name: string;
-    price: number;
-    check: boolean;
-    sheet_name: string;
-    sheet_link: string;
-    sheet_id: number;
-  }[]
+type usersType = {
+  name: string;
+  price: number;
+  check: boolean;
+  sheet_name: string;
+  sheet_link: string;
+  sheet_id: number;
+}[]
+
+const url = 'https://script.googleusercontent.com/a/macros/bita.jp/echo?user_content_key=0eE1jmrpuK-mcd2xL4uW_puwAqQWep0EpXaK7VssmP0L-7o6nqX8ME4V0mW-LTMfTLqxmRjw93-aNvl7tlVCwEA0p8Nl-joVm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_nRPgeZU6HP8d3hra3w8XUBVQhQ28DnQkJQL9R93UYiN6yYwVnDGfdKav5zPrJTegw-dYDdJIo-13eZTaZqANCXp3ctOaegirzTwHKX3qPpEq_pzm8kx7G50JyARCyWU63P0nD0x3y64&lib=Mku0QH6I7NxU3vji3EUa3jJJlMbEl9Nie'
+
+const getUsersNotPay = async (): Promise<usersType> => {
+  const data = await fetch(url)
+  const response = await data.json()
+  return response
 }
 
-export const SheetsPage = ({ users }: Props) => {
+export const SheetsPage = () => {
+  const [users, setUsers] = useState<usersType>([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true)
+      const users: usersType = await getUsersNotPay()
+      setIsLoading(false)
+      setUsers(users)
+    }
+    getData()
+
+  }, [])
+
+  const renderLoading = Array.from({ length: 20 }, (_, index) => (
+    <Fragment key={index}>
+      <CircularProgress color="secondary" />
+      <CircularProgress color="success" />
+      <CircularProgress color="inherit" />
+      <CircularProgress color="secondary" />
+      <CircularProgress color="success" />
+      <CircularProgress color="inherit" />
+      <CircularProgress color="secondary" />
+      <CircularProgress color="success" />
+      <CircularProgress color="inherit" />
+      <CircularProgress color="secondary" />
+      <CircularProgress color="success" />
+      <CircularProgress color="inherit" />
+    </Fragment>
+  ))
+
   return (
     <Container>
       <Stack gap={3}>
+        {isLoading &&
+          <Box display='flex' gap={2} flexWrap='wrap'>
+            {renderLoading}
+          </Box>}
         {users.map((user, index) => (
           <Stack key={index} gap={1} p={2} borderRadius={4} bgcolor={'#cccccc50'}>
             <Typography>
@@ -25,7 +67,7 @@ export const SheetsPage = ({ users }: Props) => {
               ơi, A/C còn
               <Typography component='span' fontSize={24} color={'red'}> {convertToVND(user.price)} </Typography>
               có vẻ như chưa trả ở name sheet: {user.sheet_name}</Typography>
-            <Typography>Link sheet ở đây nha A/C 
+            <Typography>Link sheet ở đây nha A/C
               <Link href={`${user.sheet_link}#gid=${user.sheet_id}`} target='_blank'>
                 Link Full HD (K che)
               </Link>
